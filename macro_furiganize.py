b2c99b676033f9157cb0060a8d52f7179ea88006
Aleksandr Drozd <alexander.drozd@gmail.com>
2025-06-28 19:23:28 +0900

    update samples

--- macro_furiganize.py
+++ macro_furiganize.py
@@ -335,8 +335,8 @@ if __name__ == "__main__":
     samples.append("手紙.")
     samples.append("手紙。")
     samples.append("カリン、 千葉 千葉 千 彼二千三百六十円も使った。回転寿司.")
-    samples.append("私は日本人です")
-    samples.append("水田をみる.水をのむ.")
+    samples.append("私は学生です。")
+    samples.append("水田がある.水をのむ.")
     for expr in samples:
         print("-------------------")
         print("parsing expression:", expr)
